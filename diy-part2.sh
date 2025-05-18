2fed641dea42b9e4c7233c78010a6a854e5b9f78
JDKCHINA <935341417@qq.com>
2025-05-18 10:31:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,9 +21,3 @@
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,netdata,haproxy,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 rm -rf feeds/luci/applications/luci-app-alist
 rm -rf feeds/luci/packages/net/alist
-rm -rf package/feeds/packages/gnutls
-rm -rf package/feeds/packages/containerd
-rm -rf package/feeds/packages/alist
-rm -rf package/kernel/mac80211
-rm -rf package/feeds/packages/smartmontools
-rm -rf package/feeds/packages/ruby
