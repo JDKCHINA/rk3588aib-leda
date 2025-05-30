41f403a9cca88ffb5f414d3e4fc9fdfa4e5a1dbc
JDKCHINA <935341417@qq.com>
2025-05-30 14:34:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,10 +12,10 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i 's/coolsnowwolf\/luci.git;openwrt-23.05/coolsnowwolf\/luci.git/g' feeds.conf.default
+#sed -i 's/coolsnowwolf\/luci.git;openwrt-23.05/coolsnowwolf\/luci.git/g' feeds.conf.default
 # Add a feed source
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 #sed -i '$a src-git modem https://github.com/JDKCHINA/QModem.git;main' feeds.conf.default
-git clone https://github.com/FUjr/QModem package/luci-app-qmodem
-git clone https://github.com/JiaY-shi/fancontrol package/fancontrol
+#git clone https://github.com/FUjr/QModem package/luci-app-qmodem
+#git clone https://github.com/JiaY-shi/fancontrol package/fancontrol
 
